5de7eacddfebb48b6bcca1d0270db8bfdd6ec5fa
Sendrik <o.yuryeva@ukma.edu.ua>
2019-06-17 22:47:16 +0300

    Disabled csrf

--- src/main/java/com/naukma/soccer/spring_security/SecurityConfig.java
+++ src/main/java/com/naukma/soccer/spring_security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .and()
                 .formLogin()
